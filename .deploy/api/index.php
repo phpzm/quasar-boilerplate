38bca7afa6cdc5cb30eec3a2bf393098ba02ebc6
William Correa <wilcorrea@gmail.com>
2017-10-30 17:04:16 -0200

    Update index.php

--- .deploy/api/index.php
+++ .deploy/api/index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
 
 header("Access-Control-Expose-Headers: Authorization");
 header("Content-Type: application/json");
-header("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QvYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE1MDg4OTg5OTYsImV4cCI6MTUwODkwMjU5NiwibmJmIjoxNTA4ODk4OTk2LCJqdGkiOiI5WHNOcGpickdVeXdGVGlvIn0.QH4Y5DurBTgqekfyjx3ssdduqKBMIH5zeZztRkJdZI0");
+header("Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QvYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE1MDg4OTg5OTYsImV4cCI6MTUwODkwMjU5NiwibmJmIjoxNTA4ODk4OTk2LCJqdGkiOiI5WHNOcGpickdVeXdGVGlvIn0.QH4Y5DurBTgqekfyjx3ssdduqKBMIH5zeZztRkJdZI0");
 
 echo json_encode([
     'body' => [
