842166a1803afe21b12489f546797c657563fb5b
William Correa <wilcorrea@gmail.com>
2017-10-31 10:56:46 -0200

    Teste pelo ícone da rota para determinar se a rota aparece ou não no breadcrumb

--- src/app/components/layout/Breadcrumb.vue
+++ src/app/components/layout/Breadcrumb.vue
@@ -50,7 +50,7 @@
         Object.keys(this.$route.params).forEach(key => {
           to = to.replace(':' + key, this.$route.params[key])
         })
-        if (match.meta.label) {
+        if (match.meta.icon) {
           accumulate.push({
             icon: match.meta.icon,
             label: match.meta.label,
