d93971cae41e77347e645b1c18854ef072d2f61e
William Correa <wilcorrea@gmail.com>
2017-10-31 13:36:03 -0200

    Centralizar o form na tela

--- src/domains/auth/credential/components/Login.vue
+++ src/domains/auth/credential/components/Login.vue
@@ -9,7 +9,7 @@
       src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67"
       alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"></a>
 
-    <div class="card-wrapper">
+    <div class="card-wrapper flex justify-center items-center">
 
       <q-card class="card-form">
         <q-card-media>
