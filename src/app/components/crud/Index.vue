adc1134398258c4fdccffb65ef2d9505b1775221
William Correa <wilcorrea@gmail.com>
2017-11-01 23:10:19 -0200

    Novos componentes de CRUD

---
+++ src/app/components/crud/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <app-card>
+    <div slot="title">
+      <slot name="header">
+        {{ title }}
+      </slot>
+    </div>
+    <router-view slot="content"></router-view>
+  </app-card>
+
+</template>
+
+<script type="text/javascript">
+  import AppCard from 'src/app/components/card/AppCard.vue'
+
+  export default {
+    components: {
+      AppCard
+    },
+    name: 'app-base',
+    props: {
+      title: {
+        default: ''
+      }
+    }
+  }
+</script>
