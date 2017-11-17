b2a20a113894b71385f244e17d17604cfc2625f5
William Correa <wilcorrea@gmail.com>
2017-11-17 09:11:28 -0200

    Pequenos ajustes para adaptação para a v0.0.6 do genesis

--- src/bootstrap/configure/buttons.js
+++ src/bootstrap/configure/buttons.js
@@ -4,67 +4,183 @@ import { view, edit, add, create, read, update, destroy } from 'src/bootstrap/co
 const color = 'positive'
 
 /**
- * @param {Object} $this
+ * @param {AppCrudGrid|AppCrudForm} $this
  * @return {Array}
  */
 export default ($this) => {
   return [
     // ********************************* middle *********************************
 
-    button('view', 1, 'Detalhes', view($this), 'search', 'Visualize detalhes deste registro').$middle(),
-
-    button('edit', 3, 'Editar', edit($this), 'edit', 'Edite este registro', 'primary').$middle(),
-
-    button('destroy', 4, 'Apagar', destroy($this, null, 'Confirmação', 'Deseja apagar este registro?'), 'delete',
-      'Envie este registro para a lixeira', 'negative').$middle(),
+    buttonView($this).$middle(),
+    buttonEdit($this).$middle(),
+    buttonDestroy($this).$middle(),
 
     // ********************************* grid *********************************
 
-    button('refresh', 1, '', read($this, true), 'refresh', 'Recarregar os dados', 'primary').$grid(),
-
-    button('add-inline', 2, '', add($this), 'add', 'Inicie a criação de um novo registro', 'primary').$grid(),
-
-    button('retry', 1, 'Tentar novamente', () => $this.read(), 'add',
-      'Realizar uma nova tentativa de recuperação de dados', 'primary').$render(['retry'], ['center']),
-
-    button('first', 2, 'Criar um novo registro', add($this), 'add', 'Inicie a criação de um novo registro',
-      'primary').$render(['empty'], ['center']),
+    buttonRefresh($this).$grid(),
+    buttonAddInline($this).$grid(),
+    buttonRetry($this).$render(['retry'], ['center']),
+    buttonFirst($this).$render(['empty'], ['center']),
 
     // ********************************* form *********************************
 
-    button('create', 2, 'Salvar', create($this), '', 'Salvar as alterações feitas a este registro', 'primary')
-      .$validate().$form(['create']),
-
-    button('update', 3, 'Salvar', update($this), '', 'Salvar as alterações feitas a este registro', 'primary')
-      .$validate().$form(['edit']),
-
-    button('save-and-add', $this.scope === 'create' ? 2 : 3, 'Salvar & Novo',
-      (record) => $this.save(record, () => $this.browse($this.path + '/' + 'create')), '',
-      'Salva este registro e abre nova tela para criação de um novo registro').$validate().$form(['create', 'edit']),
-
-    button('save-and-back', $this.scope === 'create' ? 2 : 3, 'Salvar & Voltar',
-      (record) => $this.save(record, () => $this.browse($this.path)), '',
-      'Salva este registro e volta para a lista principal de registros').$validate().$form(['create', 'edit']),
-
-    button('editing', 3, 'Editar', edit($this), '', 'Editar este registro').$form(['view']),
-
-    button('destructing', 4, 'Apagar', destroy($this, () => {
-      $this.browse($this.path)
-    }, 'Confirmação', 'Deseja apagar este registro?'), '', 'Apagar este registro').$form(['view', 'edit']),
+    buttonCreate($this).$validate().$form(['create']),
+    buttonUpdate($this).$validate().$form(['edit']),
+    buttonSaveAndAdd($this).$validate().$form(['create', 'edit']),
+    buttonSaveAndBack($this).$validate().$form(['create', 'edit']),
+    buttonEditInline($this).$form(['view']),
+    buttonDestroyInline($this).$form(['view', 'edit']),
 
     // ********************************* general *********************************
 
-    button('add', 2, '', add($this), 'add', 'Inicie a criação de um novo registro', 'primary')
-      .$all(['floating'], {round: true}),
-
-    button('back', 1, 'Voltar', () => $this.$router.go(-1), '', 'Voltar para a tela anterior').$all(['top', 'bottom']),
+    buttonAdd($this).$all(['floating']),
+    buttonBack($this).$all(['top', 'bottom']),
 
     // ********************************* last buttons *********************************
 
-    button('start', 1, '', () => $this.browse($this.path, false), 'apps', 'Abre a lista de registros', color)
-      .$options({rotate: false}).$form(),
-
-    button('filter', 1, '', () => $this.filterOpen(), 'filter_list', 'Abre o painel de filtros', color)
-      .$options({rotate: false, 'class': '--button-filter'}).$grid()
+    buttonStart($this).$form(),
+    buttonFilter($this).$grid()
   ]
 }
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonView = $this => button(
+  'view', 1, 'Detalhes', view($this), 'search', 'Visualize detalhes deste registro'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonEdit = $this => button(
+  'edit', 3, 'Editar', edit($this), 'edit', 'Edite este registro', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonDestroy = $this => button(
+  'destroy', 4, 'Apagar', destroy($this, 'Confirmação', 'Deseja apagar este registro?'), 'delete',
+  'Envie este registro para a lixeira', 'negative'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonRefresh = $this => button(
+  'refresh', 1, '', read($this, true), 'refresh', 'Recarregar os dados', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonAddInline = $this => button(
+  'add-inline', 2, '', add($this), 'add', 'Inicie a criação de um novo registro', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonRetry = $this => button(
+  'retry', 1, 'Tentar novamente', () => $this.read(), 'add', 'Realizar uma nova tentativa de recuperação de dados',
+  'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonFirst = $this => button(
+  'first', 2, 'Criar um novo registro', add($this), 'add', 'Inicie a criação de um novo registro', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonCreate = $this => button(
+  'create', 2, 'Salvar', create($this), '', 'Salvar as alterações feitas a este registro', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonUpdate = $this => button(
+  'update', 3, 'Salvar', update($this), '', 'Salvar as alterações feitas a este registro', 'primary'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonSaveAndAdd = $this => button(
+  'save-and-add', $this.scope === 'create' ? 2 : 3, 'Salvar & Novo',
+  record => $this.save(record, () => $this.browse($this.path + '/' + 'create')), '',
+  'Salva este registro e abre nova tela para criação de um novo registro'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonSaveAndBack = $this => button(
+  'save-and-back', $this.scope === 'create' ? 2 : 3, 'Salvar & Voltar',
+  record => $this.save(record, () => $this.browse($this.path)), '',
+  'Salva este registro e volta para a lista principal de registros'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonEditInline = $this => button('edit-inline', 3, 'Editar', edit($this), '', 'Editar este registro')
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonDestroyInline = $this => button(
+  'destroy-inline', 4, 'Apagar',
+  destroy($this, 'Confirmação', 'Deseja apagar este registro?', () => $this.browse($this.path)),
+  '', 'Apagar este registro'
+)
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonAdd = $this => button(
+  'add', 2, '', add($this), 'add', 'Inicie a criação de um novo registro', 'primary'
+).$options({round: true})
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonBack = $this => button(
+  'back', 1, '', () => $this.$router.go(-1), 'reply', 'Voltar para a tela anterior'
+).$options({rotate: false})
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonStart = $this => button(
+  'start', 1, '', () => $this.browse($this.path, false), 'fa-level-up', 'Abre a lista de registros', color
+).$options({rotate: false})
+
+/**
+ * @param {AppCrudGrid|AppCrudForm} $this
+ * @returns {Object}
+ */
+export const buttonFilter = $this => button(
+  'filter', 1, '', () => $this.filterOpen(), 'fa-filter', 'Abre o painel de filtros', color
+).$options({rotate: false, 'class': '--button-filter'})
--- src/vendor/quasar/imports.js
+++ src/vendor/quasar/imports.js
@@ -38,6 +38,7 @@ export {
   QDataTable,
   QSlider,
   QModal,
+  QModalLayout,
   QChip,
   QPagination,
   QTab,
