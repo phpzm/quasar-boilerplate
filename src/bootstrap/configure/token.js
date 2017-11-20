d47f2198a9b3b906f211bcfb520c40223650b5e3
William Correa <wilcorrea@gmail.com>
2017-11-19 23:36:57 -0200

    Adaptações para as mudanças da v0.0.10

--- src/bootstrap/configure/token.js
+++ src/bootstrap/configure/token.js
@@ -1,7 +1,10 @@
+import { set } from 'lodash'
+
 /**
+ * @param {Axios} http
  * @param {string} token
  * @returns {string}
  */
-export default (token) => {
-  return `${token}` // `Bearer ${token}`
+export default (http, token) => {
+  set(http, 'defaults.headers.common.Authorization', token) // `Bearer ${token}`
 }
